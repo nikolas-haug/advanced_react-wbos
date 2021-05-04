fdfa7909a16471eec0e3b99131b18c9a1b61d47c
Nikolas Haug <nikohaug21@gmail.com>
2021-05-04 16:27:36 -0500

    add product cards and format money util

---
+++ frontend/lib/formatMoney.js
@@ -0,0 +1,17 @@
+export default function formatMoney(amount = 0) {
+    const options = {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+    }
+
+    // check if it's a clean dollar amount
+    if(amount % 100 === 0) {
+        console.log('even price amount!');
+        options.minimumFractionDigits = 0;
+    }
+
+    const formatter = new Intl.NumberFormat('en-US', options);
+
+    return formatter.format(amount / 100);
+}
